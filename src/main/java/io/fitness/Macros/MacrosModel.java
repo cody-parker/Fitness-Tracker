b8fecdccb41f31864766a6058f7e18f15abbaeb3
Cody <Codyp@shop.com>
2021-04-17 19:12:08 -0600

    Added POM.xml and MacrosModel

---
+++ src/main/java/io/fitness/Macros/MacrosModel.java
@@ -0,0 +1,51 @@
+package io.fitness.Macros;
+
+public class MacrosModel {
+
+    private int calories;
+    private int carbs;
+    private int fats;
+    private int protein;
+
+    public MacrosModel(){}
+
+    public MacrosModel(int calories, int carbs, int fats, int protein){
+        this.calories = calories;
+        this.carbs = carbs;
+        this.fats = fats;
+        this.protein = protein;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(int carbs) {
+        this.carbs = carbs;
+    }
+
+    public int getFats() {
+        return fats;
+    }
+
+    public void setFats(int fats) {
+        this.fats = fats;
+    }
+
+    public int getProtein() {
+        return protein;
+    }
+
+    public void setProtein(int protein) {
+        this.protein = protein;
+    }
+
+}
